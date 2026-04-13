01ec1c6eb5c53fe46cbc1fb27c1693ace02b863a
Sydney omori <sydneyomori@gmail.com>
2026-04-13 13:09:20 +0300

    create js functionality

--- index.js
+++ index.js
@@ -45,3 +45,26 @@ const bookStore = {
 
 // Write your code here!
 
+const bookStoreTitle = document.getElementById('header');
+bookStoreTitle.textContent = bookStore.name;
+
+
+
+
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement('li');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const bookImage = document.createElement('img');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    document.getElementById('book-list').appendChild(bookContainer);
+});
+
